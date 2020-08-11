69fdbab39fb41bc824f64611a6f60b4373efd350
artisan <cjnum1@outlook.com>
2020-08-11 22:13:29 +0800

    Spring实战第二章-taco-cloud 1、Taco类、Order类添加属性验证 2、DesignTacoController 添加error判断跳转 3、OrderController 添加error判断跳转 4、orderForm.html 部分属性验证未起作用，待调整

--- src/main/java/armo/tacocloud/web/DesignTacoController.java
+++ src/main/java/armo/tacocloud/web/DesignTacoController.java
@@ -6,10 +6,12 @@ import armo.tacocloud.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -51,8 +53,13 @@ public class DesignTacoController {
         return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
     }
 
+    //  @Valid注解 用于获取请求时，对类Taco对象进行属性值校验
     @PostMapping
-    public String processDesign(Taco design){
+    public String processDesign(@Valid Taco design, Errors errors){
+        if (errors.hasErrors()){
+            return "design";
+        }
+
         log.info("Process design: " + design);
 
         // 重定向视图
